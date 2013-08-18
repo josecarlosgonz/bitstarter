b80acdf232d327e74155645453bade461fba87a9
Jose Gonzalez <josecarlosgonz@gmail.com>
2013-08-18 22:39:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,12 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer = new Buffer( fs.readFileSunc('index.html', function(err,data){
-  if(err)throw err;
-  console.log(data) 
-}),"utf-8")
-
-
 app.get('/', function(request, response) {
   response.send('Hello world 2.0!');
 });
