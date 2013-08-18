7646c2ee7019bd16530a2ddac2e6b307e19f6385
Jose Gonzalez <josecarlosgonz@gmail.com>
2013-08-18 22:30:11 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer = new Buffer( fs.readFileSunc('index.html', function(err,data){
+  if(err)throw err;
+  console.log(data) 
+}),"utf-8")
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2.0!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
