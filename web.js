8ff330bee6ddb04efd58a8739a3ce9af485ccc01
Jose Gonzalez <josecarlosgonz@gmail.com>
2013-08-18 22:58:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 var fs = require('fs');
-var message = fs.readFileSync('index.html','utf-8');
+var message = buf.toString(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
   response.send(message);
